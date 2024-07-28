e302ef30b3ca1449d93175c99fcb6599957a2f80
Robert Tiffert <robert@tiffert.eu>
2024-07-28 10:26:29 +0000

    Reconfigure settings.py for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ['8000-robizman-djangoblogwalk-2ja0ocrith3.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = []
+
+CSRF_TRUSTED_ORIGINS = []
+
+host = os.environ.get("HOST")
+if host:
+    ALLOWED_HOSTS.append(host)
+    CSRF_TRUSTED_ORIGINS.append(f"https://{host}")
 
 # Application definition
 
@@ -87,11 +94,6 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeinstitute-ide.net",
-    "https://*.herokuapp.com"
-]
 
 
 # Password validation
